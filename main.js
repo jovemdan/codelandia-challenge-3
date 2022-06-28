842874c03ae3f8598dbfc6bd3ae479078233ce89
Danilo Menezes <danilo.menezes@guide.com.br>
2022-06-28 18:31:31 -0300

    feat: add script ScrollReveal

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+    #section-header,
+    #section-header .content-title,
+    #section-header .image,
+    #section-cards,
+    #section-cards .card,
+    #contact,
+    #contact .form`);
